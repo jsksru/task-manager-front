b8f7e499d897bfc3290f1e61aacf0cdf0b2a0a51
jsksru <mail@jsks.ru>
2020-09-07 11:54:37 +0500

    added task types, enums, interfaces

---
+++ src/store/types/TaskTypes.ts
@@ -0,0 +1,28 @@
+export enum Status {
+  STARTED,
+  INPROCESS,
+  DONE,
+  CANCEL
+}
+
+export type Task = {
+  id: string,
+  title: string,
+  started: Date,
+  expire: Date,
+  status: Status
+}
+
+export type Tasks = Task[];
+
+export interface TaskState {
+  tasks: {
+    list: Tasks,
+    isLoading: boolean
+  }
+}
+
+export type NewTask = {
+  title: string,
+  expire: Date
+}
