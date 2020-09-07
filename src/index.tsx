a3bfc2aa33301f4061f7c80aa2a7652a578800c1
jsksru <mail@jsks.ru>
2020-09-07 17:39:07 +0500

    added store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducers';
+
+const store = createStore(rootReducer);
 
 render(
-  <App/>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
