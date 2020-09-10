8f3f929ffe0ac92d9a3be45f8f40d955892b6119
jsksru <mail@jsks.ru>
2020-09-10 20:07:27 +0500

    styled and added more logic

--- src/components/NewTask/NewTask.tsx
+++ src/components/NewTask/NewTask.tsx
@@ -1,29 +1,87 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addTask } from '../../store/actions/TaskActions';
-import moment from 'moment';
+import DateFnsUtils from '@date-io/date-fns';
+import ruLocale from "date-fns/locale/ru";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Fade from '@material-ui/core/Fade';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import { DateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 
 interface NewTask {
   taskTitle: string
 }
 
 const NewTask = () => {
-  const now: Date = new Date();
   const [ taskTitle, setTaskTitle ] = useState('');
-  const [ taskExpire, setTaskExpire ] = useState(now);
+  const [ open, setOpen ] = useState(false);
+  const [ inputError, setinputError] = useState(false);
+  const [ taskExpire, setTaskExpire ] = useState<Date | null>(new Date());
   const dispatch = useDispatch();
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const addTaskHandler = () => {
-    dispatch(addTask(taskTitle, new Date(taskExpire)));
+
+    if (taskTitle.length === 0) {
+      setinputError(true);
+    } else {
+      setinputError(false);
+      dispatch(addTask(taskTitle, taskExpire));
+      setTaskTitle('');
+      setTaskExpire(new Date());
+      setOpen(false);
+    }
+    
   }
 
   return (
     <>
-      <span>Title:</span><br/>
-      <input type="text" placeholder="Enter task title here..." value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)}/><br/>
-      <span>Expire:</span><br/>
-      <input type="datetime-local" value={moment(taskExpire).format('YYYY-MM-DDTHH:mm')} onChange={(e) => setTaskExpire(new Date(Date.parse(e.target.value)))}/><br/>
-      <button onClick={addTaskHandler}>Add</button>
+    <Button variant="outlined" color="primary" onClick={handleClickOpen}>Добавить задачу</Button>
+    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" scroll="body" TransitionComponent={Fade}>
+      <DialogTitle id="form-dialog-title">Новая задача</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Введите название для новой задачи и установите время её окончания.
+        </DialogContentText>
+        <Box>
+          <TextField fullWidth
+                     required
+                     variant="filled"
+                     label="Что нужно зделать?"
+                     value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)}
+                     error={inputError}
+          />
+          {inputError && <FormHelperText error={inputError}>Это обязательное поле !</FormHelperText>}
+        </Box>
+        <Box marginTop={3}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ruLocale}>
+            <DateTimePicker format="d MMMM Y г. в H:mm" label="Дата и время окончания" fullWidth value={taskExpire} ampm={false} variant="inline" inputVariant="filled" onChange={setTaskExpire}/>
+          </MuiPickersUtilsProvider>
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>
+          Отмена
+        </Button>
+        <Button onClick={addTaskHandler} color="primary" variant="contained">
+          Добавить
+        </Button>
+      </DialogActions>
+    </Dialog>
     </>
   );
 }
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,29 +1,92 @@
 import React from 'react';
 import moment from 'moment';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { Tasks } from '../../store/types/TaskTypes';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/types';
 import { getStatus } from '../../utils/functions';
+import { useDispatch } from 'react-redux';
+import { removeTask } from '../../store/actions/TaskActions';
 
 moment.locale('ru');
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 const TaskList = () => {
   const tasks: Tasks = useSelector( (state: RootState) => state.TaskReducer.tasks);
   const isLoading: boolean = useSelector( (state: RootState) => state.TaskReducer.isLoading);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const deleteTask = (id: string) => {
+    dispatch(removeTask(id));
+  };
 
   const loading = (
     <div>
       Tasks Loading...
     </div>
   );
-
-  const tasksList = (
-    <ul>
-      { tasks.map(task => <li key={task.id}>{task.title} exp: {moment(task.expire).format('D MMMM YYYY в H:mm')} status: {getStatus(task.status)}</li>) }
-    </ul>
+  const emptyTasks = (
+    <div>
+      Нет задач.
+    </div>
   );
 
-  return isLoading ? loading : tasksList;
+  const tasksList = (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>№</TableCell>
+            <TableCell align="left">Задача</TableCell>
+            <TableCell align="right">Создана</TableCell>
+            <TableCell align="right">Истекает</TableCell>
+            <TableCell align="right">Статус</TableCell>
+            <TableCell align="right">Действия</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tasks.map((task) => (
+            <TableRow key={task.id}>
+              <TableCell component="th" scope="row">{task.id}</TableCell>
+              <TableCell align="left">{task.title}</TableCell>
+              <TableCell align="right">{moment(task.started).format('D MMM в H:mm')}</TableCell>
+              <TableCell align="right">{moment(task.expire).format('D MMM в H:mm')}</TableCell>
+              <TableCell align="right">
+                <Chip label={getStatus(task.status)} />
+              </TableCell>
+              <TableCell align="right">
+                <IconButton color="primary" size="small" aria-label="Edit this record" component="button">
+                  <EditIcon />
+                </IconButton>
+                <IconButton color="secondary" size="small" aria-label="Delete this record" component="button" onClick={() => deleteTask(task.id)}>
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+
+  return isLoading ? loading : (tasks.length > 0) ? tasksList : emptyTasks;
 }
 
 export default TaskList;
